55b13b3766bb58dd3161e5cbda73ab859eeb789a
HakimaAD <hakimaayed20@gmail.com>
2025-11-14 01:38:25 +0100

    Initial commit: MBD_Vitesse project

---
+++ models/MBD_SpeedControl_ert_rtw/MBD_SpeedControl_private.h
@@ -0,0 +1,50 @@
+/*
+ * File: MBD_SpeedControl_private.h
+ *
+ * Code generated for Simulink model 'MBD_SpeedControl'.
+ *
+ * Model version                  : 1.5
+ * Simulink Coder version         : 24.1 (R2024a) 19-Nov-2023
+ * C/C++ source code generated on : Fri Nov 14 01:31:17 2025
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef MBD_SpeedControl_private_h_
+#define MBD_SpeedControl_private_h_
+#include "rtwtypes.h"
+#include "MBD_SpeedControl_types.h"
+#include "rtw_continuous.h"
+#include "rtw_solver.h"
+
+/* Private macros used by the generated code to access rtModel */
+#ifndef rtmIsMajorTimeStep
+#define rtmIsMajorTimeStep(rtm)        (((rtm)->Timing.simTimeStep) == MAJOR_TIME_STEP)
+#endif
+
+#ifndef rtmIsMinorTimeStep
+#define rtmIsMinorTimeStep(rtm)        (((rtm)->Timing.simTimeStep) == MINOR_TIME_STEP)
+#endif
+
+#ifndef rtmSetTPtr
+#define rtmSetTPtr(rtm, val)           ((rtm)->Timing.t = (val))
+#endif
+
+/* Used by FromWorkspace Block: '<S2>/fromWS_Signal 1' */
+#ifndef rtInterpolate
+# define rtInterpolate(v1,v2,f1,f2)    (((v1)==(v2))?((double)(v1)): (((f1)*((double)(v1)))+((f2)*((double)(v2)))))
+#endif
+
+#ifndef rtRound
+# define rtRound(v)                    ( ((v) >= 0) ? floor((v) + 0.5) : ceil((v) - 0.5) )
+#endif
+#endif                                 /* MBD_SpeedControl_private_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
---
+++ models/MBD_SpeedControl_ert_rtw/MBD_SpeedControl_types.h
@@ -0,0 +1,28 @@
+/*
+ * File: MBD_SpeedControl_types.h
+ *
+ * Code generated for Simulink model 'MBD_SpeedControl'.
+ *
+ * Model version                  : 1.5
+ * Simulink Coder version         : 24.1 (R2024a) 19-Nov-2023
+ * C/C++ source code generated on : Fri Nov 14 01:31:17 2025
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef MBD_SpeedControl_types_h_
+#define MBD_SpeedControl_types_h_
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_MBD_SpeedControl_T RT_MODEL_MBD_SpeedControl_T;
+
+#endif                                 /* MBD_SpeedControl_types_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
